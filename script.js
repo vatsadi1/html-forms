bae5eb75cac34b6e690639ea283e42dc78d4b1e2
Aditya kumar mishra <vatsadi9060@gmail.com>
2024-02-06 20:36:07 +0530

    first commit by aditya mishra

---
+++ script.js
@@ -0,0 +1,10 @@
+console .log ("hello aditya");
+var a=50;
+var b=89;
+console.log(a+b)
+var h=document.getElementById("sh")
+
+var g=document.getElementById("sh")
+const fun = ()=>{
+    h.innerHTML=a+b;
+}
